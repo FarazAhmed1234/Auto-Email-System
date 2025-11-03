004f8ca0de009e58dca04652d75ea8d8869de347
Faraz <SP24BSCS0044@maju.edu.pk>
2025-11-03 19:51:54 +0500

    auto-email-system

--- backend/server.js
+++ backend/server.js
@@ -115,8 +115,6 @@ app.delete("/api/students/:id", (req, res) => {
 
 
 // ==========================
-// ✉️ Send Email Function 
-// ==========================
 
 async function sendEmail(student) {
   const { student_name, student_email, supervisor_name, supervisor_email } = student;
